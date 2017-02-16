0553dcec2e4c745c4489d0af23186efa2b8eabe5
cassidylaidlaw <cassidy256@gmail.com>
2017-02-16 15:48:22 -0500

    Added basic project layout

---
+++ scripts/_path_config.py
@@ -0,0 +1,11 @@
+"""Configure the python path to provide access to the reviews package."""
+import sys
+from os.path import dirname, realpath, sep, pardir
+    
+sys.path.append(sep.join([dirname(realpath(__file__)), pardir]))
+
+# Configure logging as well while we're at it
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    level=logging.INFO)
