20645a4262797cccb127a41b8921750db53f6f11
Ephraim Rothschild <ephraimrothschild@yahoo.com>
2017-02-19 08:41:03 -0500

    Added website files

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -2,7 +2,11 @@ import _path_config
 import logging
 
 from flask import Flask, request, jsonify
+from flask import render_template
+
 app = Flask(__name__)
+app.debug = True
+
 
 logging.info('loading models...')
 
@@ -38,6 +42,37 @@ def analyze_reviews():
         return jsonify(json_clusters)
     else:
         return jsonify({})
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/review')
+def review():
+    product_url = request.args.get('url')
+    if product_url is not None:
+        logging.info('scraping reviews from %s', product_url)
+        reviews = scrape_reviews_from_url(product_url)
+        logging.info('read %d reviews from %s', len(reviews), product_url)
+        phrases = reviews_to_phrases(reviews)
+        logging.info('extracted %d phrases', len(phrases))
+        for phrase in phrases:
+            phrase.infer_vector(doc2vec)
+        eps = float(request.args.get('eps', '0.5'))
+        minpts = int(request.args.get('minpts', '5'))
+        clusters = cluster_phrases(phrases, eps, minpts)
+        logging.info('found %d clusters', len(clusters))
+
+        json_clusters = []
+        review_clusters = []
+        for cluster in clusters.values():
+            phrase_array = [' '.join(phrase.tokens) for phrase in cluster]
+            review_clusters.append((phrase_array[0], len(phrase_array, phrase_array[0].title())))
+            # json_clusters.append([phrase.to_json() for phrase in cluster])
+
+
+    return render_template('review.html', item_name="The unstoppable passage of time #entropyalwaysincreases", overall=-1, reviews=review_clusters, num_stars=4)
+
 
 if __name__ == '__main__':
     app.run()
