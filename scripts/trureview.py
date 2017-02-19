7123f6acd78cdf956402daeceb5991f91ee6bac4
Noah Bar-Shain <nbarshain@gmail.com>
2017-02-19 12:37:53 -0500

    web skeleton working

---
+++ scripts/trureview.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/review')
+def review():
+    reviews = [("this thing sucks", 10), ("I am too tired for this shit", 100000)]
+
+    return render_template('review.html', item_name="The unstoppable passage of time #entropyalwaysincreases", overall=1, reviews=reviews)
+
+
+
+if __name__ == '__main__':
+    app.run()
